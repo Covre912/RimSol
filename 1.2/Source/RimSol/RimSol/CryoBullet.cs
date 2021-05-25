2a56c0dda063159fe9e941d21d11d2f7962e67b9
Kitsune <joaopcovrerocha@gmail.com>
2021-05-25 19:04:39 -0300

    V.0.1.7 Persona Weapons

---
+++ 1.2/Source/RimSol/RimSol/CryoBullet.cs
@@ -0,0 +1,31 @@
+﻿using RimWorld;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Verse;
+
+namespace RimSol
+{
+    class CryoBullet : Bullet
+    {
+        protected override void Impact(Thing hitThing)
+        {
+            if (hitThing is Pawn hitPawn && hitPawn != null && hitPawn.RaceProps.FleshType == FleshTypeDefOf.Normal)
+            {
+                if (hitPawn.health.hediffSet.GetFirstHediffOfDef(HediffDefOf.Hypothermia) is Hediff hypo && hypo != null)
+                {
+                    hypo.Severity += 0.1f;
+                }
+                else
+                {
+                    hitPawn.health.AddHediff(HediffDefOf.Hypothermia);
+                    hitPawn.health.hediffSet.GetFirstHediffOfDef(HediffDefOf.Hypothermia).Severity = 0.2f;
+                }
+            }
+            base.Impact(hitThing);
+        }
+    }
+
+}
